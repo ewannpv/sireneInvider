fb14fa0af69ae35fc9d76e3c01c2330bc656c15a
Ewann <ewannpv@google.com>
2021-10-21 12:20:26 +0200

    Adds constants file

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const maxLinesPerFiles = 250;
+export const maxFileNumber = 10;
+export const csvFilePath = "src/data/StockEtablissement_utf8.csv";
--- src/split_csv.js
+++ src/split_csv.js
@@ -1,24 +1,24 @@
 import readline from 'readline';
 import fs from 'fs';
+import { csvFilePath, maxFileNumber, maxLinesPerFiles } from './constants/constants.js';
 
-const fileName = "./data/StockEtablissement_utf8.csv";
+console.log(csvFilePath);
 const lineNumber = 0;
 const lineReader = readline.createInterface({
-    input: fs.createReadStream(fileName),
+    input: fs.createReadStream(csvFilePath),
     output: process.stderr,
     console: false
 });
 
 let fileNumber = 0;
 let currentFile = "";
-const maxFileNumber = 10;
 let currentLine = 0;
 
 const createNewFile = () => {
     if (fileNumber++ >= maxFileNumber)
         return lineReader.close();
 
-    fs.writeFile(`./data/sample/sample-${fileNumber}`, currentFile, (err) => {
+    fs.writeFile(`src/data/sample/sample-${fileNumber}`, currentFile, (err) => {
         if (err) throw err;
         console.log('File is created successfully.');
     });
@@ -29,7 +29,7 @@ const createNewFile = () => {
 const readCurrentLine = (line) => {
     currentFile += line.toString();
     currentLine++;
-    if (currentLine % 101 == 1)
+    if (currentLine % maxLinesPerFiles == 1)
         createNewFile();
 }
 
