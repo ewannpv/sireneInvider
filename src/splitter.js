1e3a1ad178fb74d65add9c6da1df1b9ca5fa15d4
Ewann <ewannpv@gmail.com>
2021-11-24 15:20:14 +0100

    Adds some stuff

--- src/splitter.js
+++ src/splitter.js
@@ -2,6 +2,8 @@ import { splitFile } from './chunks_csv.js';
 import { checkDir } from './utils.js';
 import { workerDir } from './constants/constants.js';
 import { performance } from 'perf_hooks';
+import fs from 'fs';
+import pm2 from 'pm2';
 
 // Generates file samples for the workers.
 const generateEditedSamples = async () => {
@@ -12,6 +14,17 @@ const generateEditedSamples = async () => {
   console.log('starting');
   performance.mark('START_SPLITING');
   splitFile();
+  waitForWorkers();
 };
 
+const waitForWorkers = () => {
+  for (let index = 0; index < process.env.instances - 1; index++) {
+    fs.readdir(`${workerDir}${index}/`, (err, files) => {
+      //handling error
+      if (files.length) return;
+    });
+  }
+  performance.mark('JOB_DONE');
+  pm2.stop(process.env.pm_id);
+};
 export default generateEditedSamples;
