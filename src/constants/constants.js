7c2ca766c0470827f5d916265d496b0afc42ad59
Ewann <ewannpv@gmail.com>
2021-11-24 15:12:49 +0100

    Updates constraint

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const maxLinesPerFiles = 1000;
-export const chunkSize = 10 * 1024 * 1024;
+export const chunkSize = 1 * 1024 * 1024;
 export const workerDir = './src/data/worker/';
 export const jsonDir = './src/data/json/';
 export const csvFilePath = './src/data/StockEtablissement_utf8.csv';
