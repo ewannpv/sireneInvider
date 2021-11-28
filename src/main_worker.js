f673f4ac149dc870a55917a129f997587532f6ae
Ewann <ewannpv@gmail.com>
2021-11-28 22:03:31 +0100

    updates logs

--- src/main_worker.js
+++ src/main_worker.js
@@ -11,11 +11,6 @@ import fs from 'fs';
 let startTime;
 
 const setupMainWorker = async () => {
-  // Cleans the db.
-  console.log('Cleaning database..');
-  await mongoose.connect(mongoUrl, {});
-  await dataModel.deleteMany();
-
   startTime = Date.now();
   // Check if tmpDir exists.
   checkDir(tmpDir);
@@ -25,27 +20,28 @@ const setupMainWorker = async () => {
   chunksCSV();
 
   // Wait for workers.
-  await waitForWorkers();
-
-  performance.mark('DONE_SENDING');
-  performance.measure('EXECUTION_TIME', 'START_SPLITING', 'DONE_SENDING');
-  pm2.stop(0);
+  waitForWorkers();
 };
 
 // Wait workers to finish operations.
 const waitForWorkers = async () => {
-  let number, files;
-  do {
-    // wait 5 seconds.
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+  await mongoose.connect(mongoUrl, {});
 
-    number = await dataModel.countDocuments();
+  let number;
+  fs.watch(tmpDir, async () => {
+    const files = fs.readdirSync(tmpDir).length;
     const time = Math.round((Date.now() - startTime) / 1000);
-    console.log(`[${time} sec] number of documents added: ${number}`);
+    console.log(`[${time} sec] remaining files: ${files}`);
 
     // Check if all chunks have been processed.
-    files = fs.readdirSync(tmpDir).length;
-  } while (files > 0);
+    if (files == 0) {
+      performance.mark('DONE_SENDING');
+      performance.measure('EXECUTION_TIME', 'START_SPLITING', 'DONE_SENDING');
+      number = await dataModel.countDocuments();
+      console.log(`final number of document: ${number}`);
+      pm2.stop(0);
+    }
+  });
 };
 
 export default setupMainWorker;
